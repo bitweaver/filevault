f9137270d48d6fec0f64173bc86b97aaa626905e
Christian Fowler <spider@viovio.com>
2008-10-20 21:40:10 +0000

    BIG CHANGE: migrate p_*_edit permissions to p_*_update permission naming convention, hasEditPermission is now hasUpdatePermission, many explicit perm checks have been cleaned up to hasUpdatePermission

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -12,10 +12,6 @@ if( $gBitSystem->isPackageActive( 'filevault' ) ) {
 	define('FILEVAULT_CONTENT_TYPE_GUID',     'filevault' );
 	define('FILEVAULTFILE_CONTENT_TYPE_GUID', 'filevaultfile' );
 
-	$gBitSystem->registerAppMenu(
-		FILEVAULT_PKG_NAME, $gBitSystem->getConfig('filevault_menu_text', ucfirst( FILEVAULT_PKG_DIR ) ),
-		FILEVAULT_PKG_URL.'index.php', 'bitpackage:filevault/menu_filevault.tpl', 'File Vaults');
-
 	include_once( FILEVAULT_PKG_PATH.'FileVault.php' );
 }
 ?>
